05072d2064307334900d420681435d6198878d00
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2024-01-07 22:22:30 +0400

    Completed GetAllProductsByStore,AddProduct process functionality

--- persistence/product_repository.go
+++ persistence/product_repository.go
@@ -2,6 +2,7 @@ package persistence
 
 import (
 	"context"
+	"fmt"
 	"product-app/domain"
 
 	"github.com/jackc/pgx/v4"
@@ -11,6 +12,8 @@ import (
 
 type IProductRepository interface {
 	GetAllProducts() []domain.Product
+	GetAllProductsByStore(storeName string) []domain.Product
+	AddProduct(product domain.Product) error
 }
 
 type ProductRepository struct {
@@ -29,9 +32,40 @@ func (productRepository *ProductRepository) GetAllProducts() []domain.Product {
 	productRows, err := productRepository.dbPool.Query(ctx, "SELECT * FROM product")
 
 	if err != nil {
-		log.Error("Error while getting products %v", err)
+		log.Error("Error while getting products", err)
 	}
 	return extractProductsFromRows(productRows)
+}
+
+// !GetAllProductsByStore
+func (productRepository *ProductRepository) GetAllProductsByStore(storeName string) []domain.Product {
+	ctx := context.Background()
+
+	getProductsByStoreNameSql := `SELECT * FROM product WHERE store=$1`
+
+	productRows, err := productRepository.dbPool.Query(ctx, getProductsByStoreNameSql, storeName)
+
+	if err != nil {
+		log.Error("Failed to execute query for getting products by store name")
+	}
+	return extractProductsFromRows(productRows)
+}
+
+// !AddProduct
+func (productRepository *ProductRepository) AddProduct(product domain.Product) error {
+	ctx := context.Background()
+
+	insertProductSql := `INSERT INTO product (name,price,discount,store) VALUES ($1,$2,$3,$4)`
+
+	addNewProduct, err := productRepository.dbPool.Exec(ctx, insertProductSql, product.Name, product.Price, product.Discount, product.Store)
+
+	if err != nil {
+		log.Error("Failed to add new product", err)
+		return err
+	} else {
+		log.Info(fmt.Printf("Product add to database successfully: %s", addNewProduct))
+	}
+	return nil
 }
 
 // ?extractProductsFromRows
