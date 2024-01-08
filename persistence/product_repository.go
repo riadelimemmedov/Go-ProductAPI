28c381a08f5c0178f34292134e994ab6667c54ea
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2024-01-08 17:46:18 +0400

    Adding GetProductById,DeleteProductById and UpdateProductPrice view to infrastructure tier

--- persistence/product_repository.go
+++ persistence/product_repository.go
@@ -2,8 +2,10 @@ package persistence
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"product-app/domain"
+	"product-app/persistence/common"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -14,6 +16,9 @@ type IProductRepository interface {
 	GetAllProducts() []domain.Product
 	GetAllProductsByStore(storeName string) []domain.Product
 	AddProduct(product domain.Product) error
+	GetProductById(productId int64) (domain.Product, error)
+	DeleteProductById(productId int64) error
+	UpdateProductPrice(productId int64, newPrice float32) error
 }
 
 type ProductRepository struct {
@@ -65,6 +70,73 @@ func (productRepository *ProductRepository) AddProduct(product domain.Product) e
 	} else {
 		log.Info(fmt.Printf("Product add to database successfully: %s", addNewProduct))
 	}
+	return nil
+}
+
+// !GetProductById
+func (productRepository *ProductRepository) GetProductById(productId int64) (domain.Product, error) {
+	ctx := context.Background()
+
+	getProductById := `SELECT * FROM product WHERE id=$1`
+
+	queryRow := productRepository.dbPool.QueryRow(ctx, getProductById, productId)
+
+	var id int64
+	var name string
+	var price float32
+	var discount float32
+	var store string
+
+	scanErr := queryRow.Scan(&id, &name, &price, &discount, &store)
+
+	if scanErr != nil && scanErr.Error() == common.NOT_FOUND {
+		return domain.Product{}, errors.New(fmt.Sprintf("Product not found with id %d", productId))
+	}
+	if scanErr != nil {
+		return domain.Product{}, errors.New(fmt.Sprintf("Error while getting product with id %d", productId))
+	}
+
+	return domain.Product{
+		Id:       id,
+		Name:     name,
+		Price:    price,
+		Discount: discount,
+		Store:    store,
+	}, nil
+}
+
+// !DeleteProductById
+func (productRepository *ProductRepository) DeleteProductById(productId int64) error {
+	ctx := context.Background()
+
+	_, getErr := productRepository.GetProductById(productId)
+
+	if getErr != nil {
+		return errors.New("Product not found")
+	}
+
+	deleteProductSql := "DELETE FROM product WHERE id=$1"
+
+	_, err := productRepository.dbPool.Exec(ctx, deleteProductSql, productId)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error while delete product with id %d", productId))
+	}
+	log.Info("Product deleted successfully")
+	return nil
+}
+
+// !UpdateProductPrice
+func (productRepository *ProductRepository) UpdateProductPrice(productId int64, newPrice float32) error {
+	ctx := context.Background()
+
+	updateProductSql := `UPDATE product SET price=$1 WHERE id=$2`
+	_, err := productRepository.dbPool.Exec(ctx, updateProductSql, newPrice, productId)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error while updating product with id %d", productId))
+	}
+	log.Info("Product price update successfully")
 	return nil
 }
 
